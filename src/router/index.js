a50dc262fb65a5c056ef0f6fb37eb7680a4541b4
Victor Santiago <victorhsantiago3@gmail.com>
2020-01-08 00:41:39 -0300

    add app routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,22 +7,28 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: () => import('@/views/NotCuratedMovies.vue'),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/liked-movies',
+    name: 'liked-movies',
+    component: () => import('@/views/LikedMovies.vue'),
+  },
+  {
+    path: '/disliked-movies',
+    name: 'disliked-movies',
+    component: () => import('@/views/DislikedMovies.vue'),
+  },
+  {
+    path: '*',
+    redirect: '/',
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 export default router
