411f5fc6abd36aa8d217aa2509b6298dd217ada3
Victor Santiago <victorhsantiago3@gmail.com>
2020-01-12 00:49:47 -0300

    add components style

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
-    <div v-for="(movie, i) in moviesList" :key="i">
-      {{ movie.id }}
+  <div class="movies__list">
+    <div class="movie" v-for="(movie, i) in moviesList" :key="i">
+      <img
+        class="movie__image"
+        :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+        alt=""
+      />
+      <section class="movie__description">
+        <p class="description__title">
+          {{ movie.original_title }}
+        </p>
+        <section class="description__rates">
+          <img
+            class="rates__rate"
+            v-for="(n, i) in upvotes(movie)"
+            :key="i"
+            src="@/assets/favorite.png"
+            alt=""
+          />
+          <img
+            class="rates__rate"
+            v-for="n in downvotes(movie)"
+            :key="n + 10"
+            src="@/assets/favorite_.png"
+            alt=""
+          />
+        </section>
+      </section>
     </div>
   </div>
 </template>
@@ -9,7 +34,49 @@
 <script>
 export default {
   props: { moviesList: Array },
+  methods: {
+    upvotes(movie) {
+      return Math.round(movie.vote_average / 2)
+    },
+    downvotes(movie) {
+      return 5 - this.upvotes(movie)
+    },
+  },
 }
 </script>
 
-<style scoped></style>
+<style lang="stylus" scoped>
+.movies__list
+  display grid
+  grid-template-columns repeat(2, 1fr)
+  grid-gap .5rem
+  justify-items center
+  padding .5rem
+
+.movie
+  width 100%
+  height 175px
+  border-radius .25rem
+  overflow hidden
+  position relative
+
+.movie__image
+  width 100%
+  height 100%
+  object-fit cover
+
+.movie__description
+  width 100%
+  position absolute
+  bottom 0
+  padding .5rem
+  background linear-gradient(0deg, rgba(100,100,100,.75) 0%, rgba(255,255,255,0) 100%)
+
+.description__title
+  text-transform uppercase
+  color #FFF
+  font-weight 600
+
+.rates__rate
+  margin-right .25rem
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,135 @@
 <template>
   <div class="home">
-    <div id="nav">
-      <router-link to="/">Filmes não curados</router-link> |
-      <router-link to="/liked-movies">Filmes curtidos</router-link> |
-      <router-link to="/disliked-movies">Filmes não curtidos</router-link>
+    <div class="header" :class="{ 'header--collapsed': toggleMenu }">
+      <img
+        class="header__menu"
+        src="@/assets/menu-lateral.png"
+        alt="Botão do menu"
+        @click="toggleMenu = !toggleMenu"
+      />
+      <img
+        class="header__logo"
+        src="@/assets/logo-viva-decora.png"
+        alt="Logo Viva Decora"
+      />
     </div>
-    <slot />
+    <div class="menu" :class="{ 'menu--open': toggleMenu }">
+      <router-link class="menu__link" to="/">
+        <span @click="toggleMenu = !toggleMenu">Filmes não curados</span>
+      </router-link>
+      <router-link
+        class="menu__link"
+        @click="toggleMenu = !toggleMenu"
+        to="/liked-movies"
+      >
+        <span @click="toggleMenu = !toggleMenu">Filmes curtidos</span>
+      </router-link>
+      <router-link
+        class="menu__link"
+        @click="toggleMenu = !toggleMenu"
+        to="/disliked-movies"
+      >
+        <span @click="toggleMenu = !toggleMenu">Filmes não curtidos</span>
+      </router-link>
+    </div>
+    <div class="body" :class="{ 'body--collapsed': toggleMenu }">
+      <slot />
+    </div>
+    <div class="background">
+      <img
+        v-if="$route.name === 'home'"
+        class="background__image"
+        :src="'https://image.tmdb.org/t/p/w500/' + getCurrentMovie.poster_path"
+        alt=""
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'home',
+  data() {
+    return {
+      toggleMenu: false,
+    }
+  },
+  computed: {
+    ...mapGetters(['getCurrentMovie']),
+  },
 }
 </script>
+
+<style lang="stylus" scoped>
+.home
+  width 100vw
+  display flex
+  flex-direction column
+
+.header
+  position relative
+  display flex
+  justify-content center
+  height 5rem
+  width 100vw
+
+.header__menu
+  align-self center
+  position absolute
+  top 2rem
+  left 2rem
+  cursor pointer
+
+
+.header__logo
+  align-self center
+
+.menu
+  position fixed
+  width 0
+  height 100vh
+  top 0
+  left 0
+  display flex
+  flex-direction column
+  justify-content center
+  overflow-x hidden
+  background-color #444444
+
+.menu__link
+  margin  1rem
+  text-transform uppercase
+  font-weight 600
+  text-decoration none
+  color #fff
+  display block
+  transition 0.3s
+
+.menu--open
+  width 75vw
+
+.header--collapsed
+  margin-left 75vw
+
+.body
+  width 100%
+
+
+.body--collapsed
+  margin-left 75vw
+
+.background
+  width 100vw
+  height 100vh
+  position fixed
+  z-index -1
+  background linear-gradient(60deg, #ff1c1c  0%, #ff5656  100%)
+
+.background__image
+  width 100%
+  height 100%
+  object-fit cover
+  opacity .2
+</style>
