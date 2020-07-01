a41cf0745a3b4ee70d636696952a2f249d895872
Victor Santiago <victorhsantiago3@gmail.com>
2020-07-01 14:28:41 -0300

    Fix: fixes deploy branch

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:victorhsantiago/movie-catalog.git master:gh-pages
+git push -f git@github.com:victorhsantiago/movie-catalog.git gh-pages
 
 cd -
